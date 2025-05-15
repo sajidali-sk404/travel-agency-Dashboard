b6c2beb62b33bc4d9a5c6c05ebbaf3cd62e0211f
Sajid Ali <sajidali.sk404@gmail.com>
2025-05-15 15:51:35 +0500

    working on Create Trip form

--- app/routes/admin/create-trip.tsx
+++ app/routes/admin/create-trip.tsx
@@ -1,7 +1,35 @@
+import { ComboBoxComponent } from "@syncfusion/ej2-react-dropdowns"
 import { Header } from "components"
+import type { Route } from "./+types/create-trip";
 
 
-const createTrip = () => {
+export const loader = async () => {
+   const response = await fetch('https://restcountries.com/v3.1/all');
+   const data = await response.json();
+
+   return data.map((country:any) => ({
+    name: country.flag + country.name.common,
+    Coordinate: country.latlng,
+    value: country.name.common,
+    openStreetMap: country.map?.openStreetMap,
+   }))
+}
+
+const createTrip = ({loaderData}:Route.ComponentProps) => {
+  const countries = loaderData as Country[];
+  
+  const countryData = countries.map((country) => ({
+      text: country.name,
+      value: country.value
+
+    }
+  ))
+ 
+  const handleSubmit = async () => {} 
+  const handelChange = (key: keyof TripFormData, value:string | number) => {
+
+  }
+
   return (
     <main className="flex flex-col gap-10 pb-20 wrapper">
          <Header
@@ -10,6 +38,34 @@ const createTrip = () => {
     />
 
     <section className="mt-2.5 wrapper-md">
+      <form action="" className="trip-form" onSubmit={handleSubmit}>
+        <label htmlFor="country">
+          Country
+        </label>
+        <ComboBoxComponent 
+          id="country"
+          dataSource={countryData}
+          fields={{text: 'text', value: 'value'}}
+          placeholder="Select a Country"
+          className="combo-box"
+          change={(e:{value:string | undefined}) => {
+                       if(e.value){
+                        handelChange('country', e.value);
+                       }}
+                      }
+                      allowFiltering
+                      filtering={(e) => {
+                        const query = e.text.toLowerCase();
+
+                        e.updateData(
+                          countries.filter((country) => country.name.toLowerCase().includes(query)).map((country) =>({
+                            text:country.name,
+                            value: country.value,
+                          }))
+                        )
+                      }}
+        />
+      </form>
 
     </section>
     </main>
